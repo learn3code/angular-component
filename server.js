34c573ae7d5b3e72a4ad02f9bdd32e3029d90e8a
Ajay <learn2code@yandex.com>
2016-07-04 23:46:55 +0000

    using expressjs

--- server.js
+++ server.js
@@ -1,86 +1,11 @@
-const fs = require("fs");
-const url = require("url");
-const http = require("http");
-const PATH = 'src';
-
+const express = require("express");
 const PORT = process.env.PORT;
 const HOST_IP = process.env.IP;
 
-const server = http.createServer(function(req, res) {
-    try {
-        getResource(req, res);
-    }
-    catch (err) {
-        console.log(err);
-    }
+var app = express();
+
+app.use(express.static('src'));
+
+app.listen(PORT, HOST_IP, function() {
+    console.log('Server started on port : ' + PORT);
 });
-
-server.listen(PORT, HOST_IP, function() {
-    console.log('Server running');
-});
-
-function getResource(req, res) {
-
-    var obj = url.parse(req.url);
-    var ct = obj.pathname.split('.')[1];
-
-    if (ct) {
-        fs.readFile(`${PATH}/${obj.pathname}`, 'utf8', function(err, data) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            res.statusCode = 200;
-            res.setHeader('Content-Type', getContentType(ct));
-            res.end(data);
-        });
-
-    }
-    else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(`<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-            <html><head>
-            <title>404 Not Found</title>
-            </head><body>
-            <h1>Not Found</h1>
-            <p>The requested URL ${req.url} was not found on this server.</p>
-            </body></html>`);
-    }
-
-    return res;
-
-};
-
-function getContentType(type) {
-    var ct = 'text/html';
-
-    switch (type) {
-        case "html":
-            ct = 'text/html';
-            break;
-        case "js":
-            ct = 'text/javascript';
-            break;
-        case 'css':
-            ct = 'text/css';
-            break;
-        case 'json':
-            ct = 'application/json';
-            break;
-        case 'gif':
-            ct = 'image/gif';
-            break;
-        case 'jpeg':
-            ct = 'image/jpeg';
-            break;
-        case 'png':
-            ct = 'image/png';
-            break;
-        default:
-            ct = 'text/text';
-    }
-
-    return ct;
-}
